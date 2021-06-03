9f0423c5c0aa02c198e870a265408578fd548541
Rixxan <rixxan@hullseals.space>
2021-06-03 13:16:13 -0400

    Prep for HalpyBOT announcer

--- fishers/index.php
+++ fishers/index.php
@@ -131,65 +131,7 @@ if (isset($_GET['send'])) {
             $stmt3->execute();
             $stmt3->close();
         }
-        $whseal = echousername($user->data()->id);
-        $whclient = $data['client_nm'];
-        $url = 'http://halpybot.hullseals.space:3141/ppwk';
-        $whpayload = array(
-          "client" => $whclient,
-          "seal" => $whseal,
-        );
-        $postdata = json_encode($whpayload);
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $webhookurl = "";
-            $timestamp = date("c", strtotime("now"));
-              $json_data = json_encode([
-                  "content" => "Paperwork Completed",
-                  "username" => "HalpyBOT",
-                  "avatar_url" => "https://hullseals.space/images/emblem_mid.png",
-                  "tts" => false,
-                  "embeds" => [
-                      [
-                          "title" => "Paperwork Complete",
-                          "type" => "rich",
-                          "timestamp" => $timestamp,
-                          "color" => hexdec( "F5921F" ),
-                          "footer" => [
-                              "text" => "Hull Seals Case Notification System",
-                              "icon_url" => "https://hullseals.space/images/emblem_mid.png"
-                          ],
-                          "fields" => [
-                              [
-                                  "name" => "Paperwork for case",
-                                  "value" => $whclient,
-                                  "inline" => true
-                              ],
-                              [
-                                  "name" => "Completed by ",
-                                  "value" => $whseal,
-                                  "inline" => true
-                              ]
-                          ]
-                      ]
-                  ]
-
-              ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-        $ch = curl_init( $webhookurl );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-        curl_setopt( $ch, CURLOPT_POST, 1);
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
-        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt( $ch, CURLOPT_HEADER, 0);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec( $ch );
-        curl_close( $ch );
+        require_once '../shout.php';
         header("Location: success.php");    }
 }
 ?>
---
+++ shout.php
@@ -0,0 +1,75 @@
+<?
+//Authenticaton Info
+$auth = require 'auth.php';
+$secret = $auth['auth'];
+$key = $auth['key'];
+$webhookurl = $auth['discord'];
+$url = $auth['url'];
+$auth = hash_hmac('sha256', $key, $secret);
+//Content
+$whseal = echousername($user->data()->id);
+$whclient = $data['client_nm'];
+// IRC Message
+$data = [
+	"type" => "PPWK",
+	"parameters" => [
+		"CMDR" => $whclient,
+		"Seal" => $whseal
+  ]
+];
+$postdata = json_encode($data);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+	'hmac: '. $auth
+));
+$result = curl_exec($ch);
+curl_close($ch);
+
+//Discord Webhook
+    $timestamp = date("c", strtotime("now"));
+      $json_data = json_encode([
+          "content" => "Paperwork Completed",
+          "username" => "HalpyBOT",
+          "avatar_url" => "https://hullseals.space/images/emblem_mid.png",
+          "tts" => false,
+          "embeds" => [
+              [
+                  "title" => "Paperwork Complete",
+                  "type" => "rich",
+                  "timestamp" => $timestamp,
+                  "color" => hexdec( "F5921F" ),
+                  "footer" => [
+                      "text" => "Hull Seals Case Notification System",
+                      "icon_url" => "https://hullseals.space/images/emblem_mid.png"
+                  ],
+                  "fields" => [
+                      [
+                          "name" => "Paperwork for case",
+                          "value" => $whclient,
+                          "inline" => true
+                      ],
+                      [
+                          "name" => "Completed by ",
+                          "value" => $whseal,
+                          "inline" => true
+                      ]
+                  ]
+              ]
+          ]
+
+      ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $ch );
+curl_close( $ch );
+?>
